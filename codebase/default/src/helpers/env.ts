082033a424f37f6f1a8ed46d4ec8860503f6b86b
Bill <fallencypher@gmail.com>
2023-05-10 20:22:15 -0400

    dotenv update to env.t3

--- codebase/default/src/helpers/env.ts
+++ codebase/default/src/helpers/env.ts
@@ -1,5 +1,7 @@
 import { createEnv } from "@t3-oss/env-core";
+import { dotenv } from "dotenv";
 import { z } from "zod";
+dotenv.config();
 
 export const env = createEnv({
   clientPrefix: "PUBLIC_",
